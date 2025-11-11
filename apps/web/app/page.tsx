ed182827730ebf636ebb16e03f3169f4799541ea
domniea <domniea@gmail.com>
2025-11-10 21:51:10 -0700

    test for preview

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   console.log('home')
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <h1 style={{color:'red'}}>test</h1>
+      {/* <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -60,7 +61,7 @@ export default function Home() {
             Documentation
           </a>
         </div>
-      </main>
+      </main> */}
     </div>
   );
 }

9e1c4f5fb3ffdc3e257142cbdd288cede73dd1d2
domniea <domniea@gmail.com>
2025-11-19 15:14:33 -0700

    working monoreop pre eas pipleines and aws

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  reactNativePath: "./apps/mobile/node_modules/react-native",
+  project: {
+    ios: {
+      project: "./apps/mobile/ios/mobile.xcworkspace",
+    },
+    android: {
+      sourceDir: "./apps/mobile/android",
+    },
+  }
+};

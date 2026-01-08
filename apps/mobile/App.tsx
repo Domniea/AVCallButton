db3d4f27e11e19ca7ba9430bd937a8d070a02591
domniea <domniea@gmail.com>
2026-01-08 14:42:16 -0700

    aws auth working on web and mobile

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -2,11 +2,10 @@ import 'react-native-gesture-handler';
 import "react-native-get-random-values";
 
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import Test from "./src/Login";
+import { StyleSheet } from "react-native";
 import { ClientProviders } from "./providers/ClientProviders";
 import RootNavigator from "./src/navigation/RootNavigator";
-import { Root } from "@chakra-ui/react/dist/types/components/accordion/namespace";
+
 
 export default function App() {
   return (

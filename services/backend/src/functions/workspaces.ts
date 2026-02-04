a6c5c30a1cb323f6c191938aacd0a37ff06fd510
domniea <domniea@gmail.com>
2026-02-04 15:20:37 -0700

    test

--- services/backend/src/functions/workspaces.ts
+++ services/backend/src/functions/workspaces.ts
@@ -1,27 +1,27 @@
-import type {
-  APIGatewayProxyHandlerV2WithJWTAuthorizer,
-} from "aws-lambda";
+// import type {
+//   APIGatewayProxyHandlerV2WithJWTAuthorizer,
+// } from "aws-lambda";
 
-export const handler: APIGatewayProxyHandlerV2WithJWTAuthorizer = async (
-  event
-) => {
-  const claims = event.requestContext.authorizer.jwt.claims;
+// export const handler: APIGatewayProxyHandlerV2WithJWTAuthorizer = async (
+//   event
+// ) => {
+//   const claims = event.requestContext.authorizer.jwt.claims;
 
-  const userId = claims.sub;
-  const email = claims.email;
+//   const userId = claims.sub;
+//   const email = claims.email;
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      userId,
-      email,
-      workspaces: [
-        {
-          workspaceId: "temp-personal",
-          name: "Personal Workspace",
-          role: "owner",
-        },
-      ],
-    }),
-  };
-};
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       userId,
+//       email,
+//       workspaces: [
+//         {
+//           workspaceId: "temp-personal",
+//           name: "Personal Workspace",
+//           role: "owner",
+//         },
+//       ],
+//     }),
+//   };
+// };
